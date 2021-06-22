b25e25eaa6758f3b34224c5296a73d450ee5f252
sridhar-5 <kv.sridharsai@gmail.com>
2021-06-22 17:57:25 +0530

    some additions

--- 2-async-patterns/2-await-patterns.js
+++ 2-async-patterns/2-await-patterns.js
@@ -1,8 +1,21 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
+
+const utils = require("util");
+
+// utils library promisify method returns a promise without even writing a p[romise
+
+// const readFilePromise = utils.promisify(readFile);
+// const writeFilePromise = utils.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await getText("../content/second.txt");
+    const first = await readFile("../content/second.txt", "utf8");
+    const second = await readFile("../content/text.txt", "utf8");
+    await writeFile(
+      "../content/result-mind-grenade.txt",
+      `This is an awesome evening : ${first} ${second}`,
+      { flag: "a" }
+    );
     console.log(first);
   } catch (e) {
     console.log(e);
@@ -24,19 +37,19 @@ start();
 
 ///this is rewriting the same code using promises;
 
-const getText = (path) => {
-  return new Promise((reject, resolve) => {
-    readFile(path, "utf8", (error, result) => {
-      if (error) {
-        console.log(error);
-        reject(error);
-      } else {
-        console.log("hmm  yeah");
-        resolve(result);
-      }
-    });
-  });
-};
+// const getText = (path) => {
+//   return new Promise((reject, resolve) => {
+//     readFile(path, "utf8", (error, result) => {
+//       if (error) {
+//         console.log(error);
+//         reject(error);
+//       } else {
+//         console.log("hmm  yeah");
+//         resolve(result);
+//       }
+//     });
+//   });
+// };
 
 // getText("../content/second.txt")
 //   .then((result) => {
