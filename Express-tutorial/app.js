78936672eed973f7487266c8be02b3e7b704a385
sridhar-5 <kv.sridharsai@gmail.com>
2021-06-25 22:10:04 +0530

    new additions

--- Express-tutorial/app.js
+++ Express-tutorial/app.js
@@ -1,10 +1,45 @@
 const express = require("express");
 const { products } = require("./data");
+
+const logger = require("./logger");
+const authorize = require("./authorize");
 const app = express();
 
-//middle ware turorial
+//middle ware tutorial
 
 // req => middleware => response
+
+//middle ware function
+
+// app.get("/", logger, (req, res) => {
+//   res.send("Home");
+// });
+
+// app.get("/about", logger, (req, res) => {
+//   res.send("About");
+// });
+//this is one way of doing this the other way
+// around is using the.use method
+
+app.use("/api", logger, authorize);
+
+//notes on app use
+
+// while creating an .use statement order definitely matters
+// like if the .use is at the top of the code it is being done for all
+// the get methods below it
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("Items");
+});
 
 app.listen(5000, () => {
   console.log("Listening to port 5000");
---
+++ realtime-trails/index.js
@@ -0,0 +1,57 @@
+//class
+const Joi = require("joi");
+//class
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "Course1" },
+  { id: 2, name: "Course2" },
+  { id: 3, name: "Course3" },
+];
+
+app.get("/", (request, response) => {
+  response.send("Hello World");
+});
+
+app.get("/api/courses", (request, response) => {
+  response.send([1, 2, 3]);
+});
+
+//post requests here
+//primary rule is to never trust what client is sending you
+// so input validation logic is a must
+
+app.post("/api/courses", (request, response) => {
+  //just a casual way of validating thhe request
+  // if (!request.body.name || request.body.name.length < 3) {
+  //   // 400 is bad request
+  //   response.status(400).send("Name is a required fiels and min of 3 chars");
+  //   return;
+  // }
+
+  //replaceing the above validation logic with joi
+  const course = {
+    id: courses.length + 1,
+    name: request.body.name,
+  };
+  courses.push(course);
+  response.send(course);
+});
+
+app.get("/api/courses/:id", (request, response) => {
+  var course = courses.find((c) => c.id === parseInt(request.params.id));
+  if (!course) {
+    response.status(404).send("course with this id is not available");
+  }
+  response.send(course);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
